1bff0762183e2549b78a7bddfe6cc1bd0a9a679b
Elvis Gomez <eogomezf@gmail.com>
2025-04-01 19:41:09 -0600

    Agregar funcionabilidad, usar lib mathjs

--- src/component/BotonClear.js
+++ src/component/BotonClear.js
@@ -1,7 +1,7 @@
 import '../css/BotonClear.css';
 
 const BotonClear = (props) => (
-    <div className='boton-clear'>
+    <div className='boton-clear' onClick={props.manejarClear} >
         {props.children}
     </div>
 );
